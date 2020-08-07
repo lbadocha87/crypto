590f6c7640989c9399d706c630cb71a74038166a
akademia108 <kursy@akademia108.pl>
2020-08-07 08:45:24 +0200

    Updates

---
+++ precache-manifest.17925f1b691d4073a86ccace5bb1ad66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373ccc6ccb5234bfa673a1fc007ba0a3",
+    "url": "/crypto/index.html"
+  },
+  {
+    "revision": "73be3ce3d211ce187a4e",
+    "url": "/crypto/static/css/main.eac2fbf4.chunk.css"
+  },
+  {
+    "revision": "e300dddb360287522cc1",
+    "url": "/crypto/static/js/2.ef495069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto/static/js/2.ef495069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73be3ce3d211ce187a4e",
+    "url": "/crypto/static/js/main.48b159cf.chunk.js"
+  },
+  {
+    "revision": "fa0d2c4ecfd7dcfd6f5f",
+    "url": "/crypto/static/js/runtime-main.85002c32.js"
+  },
+  {
+    "revision": "c6440537897819593546341d0eac5a28",
+    "url": "/crypto/static/media/logo.c6440537.svg"
+  }
+]);
